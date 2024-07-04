6ac284e001b2541ae4513631a9763bf6e2ff70ec
Miguel Santos Oliveira <miguel.santos321_@hotmail.com>
2024-07-03 21:03:35 -0300

    Finishing all enemys

---
+++ Game/Unifev_Biblioteca/Assets/Scripts/Puzzle_movement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle_movement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private Animator anim;
+    void Start()
+    {
+     anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     Vector2 move_input = new Vector2 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+     transform.Translate(move_input * speed * Time.deltaTime, Space.World);
+     flip(move_input);
+    }
+    
+        void flip(Vector2 dir)
+        {
+            if (dir.x > 0)
+            {
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+            }
+
+            else if (dir.x < 0)
+            {
+            transform.eulerAngles = Vector3.zero;
+            }
+        }
+    
+}
